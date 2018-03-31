ff24530ebbd22cb97957aeae3077878df5d299e0
Shiori <shio.yama520@gmail.com>
2018-03-31 11:16:12 +0000

    tried to compute the margin based on the window-image ratio

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
 # Todo
 
 - Make click easier
+- scale sprites and add padding
+- favorite -> transformation
 
 # Problems
 
 - Sprites can overlap
-
-- Should the camera rotation restored when leaving the focus mode?
-
-# Camera Rotation
-
-When adjusting ```camera.up```, modifying camera's rotation or its quaternion doesn't matter.
-
-Slerp method have the same rotation problem as when I didn't use slerp. Ask Roger to test with cones.
--- js/script.js
+++ js/script.js
@@ -2,36 +2,42 @@
 /* global TWEEN */
 
 // Constants
-// Colors
+// Color/Opacity
 const SCENE_BACKGROUND = 0x000000;
 const DEFAULT_SPRITE_COLOR = 0xffffff;
 const HOVERED_SPRITE_COLOR = 0xe57373;
+const VISITED_SPRITE_OPACITY = 0.5;
 // Size
+const DISTANCE_TO_FOCUSED_SPRITE = 3;
 const SPRITE_MAX_SIZE = 4;
-// Fog density
-const DEFAULT_FOG_DENSITY = 0.02;
-const FOG_DENSITY_IN_FOCUS_MODE = 0.2;
+// Sprite visibility
+const FOG_NEAR = 3;
+const DEFAULT_FOG_FAR = 50;
+const FOG_FAR_IN_FOCUS_MODE = 10;
+const RAYCASTER_FAR = 45;
 // Animation
+const FLY_CONTROLS_MOVEMENT_SPEED = 30;
 const FLY_CONTROLS_ROLL_SPEED = Math.PI / 5; // Math.PI / 6;
-const FOCUS_MODE_ANIMATION_DURATION = 800;
+const ZOOMIN_DURATION = 800;
+const ZOOMOUT_DURATION = 500;
 
 // Fly controls container
-const clock = new THREE.Clock();
 const container = document.createElement('div');
 document.body.appendChild(container);
 
 // Scene, Camera, Renderer
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(SCENE_BACKGROUND);
-scene.fog = new THREE.FogExp2(scene.background, DEFAULT_FOG_DENSITY);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.fog = new THREE.Fog(scene.background, 3, 50);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01/*DISTANCE_TO_FOCUSED_SPRITE*/, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 container.appendChild(renderer.domElement);
 
 // Fly controls
 const flyControls = new THREE.FlyControls(camera, container);
-flyControls.movementSpeed = 30;
+const clock = new THREE.Clock();
+flyControls.movementSpeed = FLY_CONTROLS_MOVEMENT_SPEED;
 const toggleFlyControlsRolling = activate => {
 	flyControls.rollSpeed = activate ? FLY_CONTROLS_ROLL_SPEED : 0;
 };
@@ -39,6 +45,7 @@ toggleFlyControlsRolling(true);
 
 // Raycaster
 const raycaster = new THREE.Raycaster();
+raycaster.far = RAYCASTER_FAR;
 let intersected; // save the previously intersected object
 const mouse = new THREE.Vector2();
 window.addEventListener('mousemove', event => {
@@ -59,13 +66,13 @@ const subwayImgNames = ['american', 'banana_peppers', 'black_forest_ham', 'black
  'mustard', 'nine_grain_wheat', 'oil', 'oven_roasted_chicken', 'pickles', 'ranch', 'red_onions', 'spinach',
  'sweet_onion', 'sweet_onion_chicken_teriyaki', 'tomatoes', 'tuna', 'turkey_breast', 'vinaigrette', 'vinegar'];
 const pepperImgNames = ['square_pepper', 'horizontal_pepper', 'vertical_pepper'];
-const spriteMaps = subwayImgNames.map(name => new THREE.TextureLoader().load(`img/${name}.png`));
+//const spriteMaps = subwayImgNames.map(name => new THREE.TextureLoader().load(`img/${name}.png`));
 
 for (let i = 0; i < 500; i++) {
 	const sprite = new THREE.Sprite(new THREE.SpriteMaterial({
-		map: spriteMaps[i % spriteMaps.length],
+		//map: spriteMaps[i % spriteMaps.length],
 		//map: spriteMaps[Math.floor(Math.random() * spriteMaps.length)],
-		/*map: new THREE.TextureLoader().load(
+		map: new THREE.TextureLoader().load(
 			`img/${pepperImgNames[i % pepperImgNames.length]}.png`,
 			texture => {
 				if (texture.image.width >= texture.image.height) {
@@ -74,19 +81,20 @@ for (let i = 0; i < 500; i++) {
 					sprite.scale.set(texture.image.width * (SPRITE_MAX_SIZE / texture.image.height), SPRITE_MAX_SIZE);
 				}
 			}
-		),*/
+		),
 		color: DEFAULT_SPRITE_COLOR,
 		fog: true
 	}));
-	sprite.scale.set(3, 3);
 	sprite.position.set(Math.random() * 100 - 50, Math.random() * 100 - 50, Math.random() * 100 - 50);
 	scene.add(sprite);
 }
 
 /*
 const sprite = new THREE.Sprite(new THREE.SpriteMaterial({color: 0xff0000}));
-sprite.position.set(-2, 5, -20);
-scene.add(sprite);*/
+sprite.position.set(0, 0, -40);
+scene.add(sprite);
+sprite.material.fog = true;
+*/
 
 /*
 const coneGeometry = new THREE.ConeGeometry(2, 5, 8);
@@ -135,34 +143,39 @@ animate();
 // Click event
 renderer.domElement.addEventListener('mousedown', event => {
   if (focusedSprite){ // zoom out
+    focus(false);
     focusedSprite = null;
-    zoom(false);
-    toggleFlyControlsRolling(true);
   } else if (intersected){ // zoom in
-  	focusedSprite.material.color.set(DEFAULT_SPRITE_COLOR);
     focusedSprite = intersected.object;
+  	focusedSprite.material.color.set(DEFAULT_SPRITE_COLOR);
     previousCameraPosition = camera.position.clone();
     previousCameraQuaternion = camera.quaternion.clone();
-    zoom(true);
-    toggleFlyControlsRolling(false);
+    focus(true);
   }
 });
 
-const zoom = zoomIn => {
+const focus = zoomIn => {
 	const tweenValues = {};
 	const tweenTarget = {};
 	
+	toggleFlyControlsRolling(!zoomIn);
+	focusedSprite.material.opacity = zoomIn ? 1 : VISITED_SPRITE_OPACITY;
+	
 	// Make the fog denser; the fog should not affect the focused sprite
-	if (zoomIn) { focusedSprite.material.fog = false; }
-	tweenValues.fogDensity = scene.fog.density;
-	tweenTarget.fogDensity = zoomIn ? FOG_DENSITY_IN_FOCUS_MODE : DEFAULT_FOG_DENSITY;
+	focusedSprite.material.fog = !zoomIn;
+	tweenValues.fogFar = scene.fog.far;
+	tweenTarget.fogFar = zoomIn ? FOG_FAR_IN_FOCUS_MODE : DEFAULT_FOG_FAR;
 	
   // Move the camera
 	const distance = new THREE.Vector3().subVectors(
 		zoomIn ? focusedSprite.position : previousCameraPosition,
 		camera.position
 	);
-	const margin = zoomIn ? distance.clone().normalize().multiplyScalar(3) : new THREE.Vector3();
+	const marginLength = computeMarginToSprite(focusedSprite.scale.y, 0.8);
+	console.log(marginLength);
+	const margin = zoomIn
+		? distance.clone().normalize().multiplyScalar(marginLength/*DISTANCE_TO_FOCUSED_SPRITE*/)
+		: new THREE.Vector3();
 	const targetPos = new THREE.Vector3().addVectors(
 		camera.position,
 		new THREE.Vector3().subVectors(distance, margin)
@@ -186,24 +199,29 @@ const zoom = zoomIn => {
   
   // Tween everything
 	const tween = new TWEEN.Tween(tweenValues)
-		.to(tweenTarget, FOCUS_MODE_ANIMATION_DURATION)
+		.to(tweenTarget, zoomIn ? ZOOMIN_DURATION : ZOOMOUT_DURATION)
 		.easing(TWEEN.Easing.Quartic.Out)
 		.onUpdate(() => {
 			// Fog
-			scene.fog.density = tweenValues.fogDensity;
+			scene.fog.far = tweenValues.fogFar;
 			
 			// Position
-			Object.assign(camera.position, {x: tweenValues.posX, y: tweenValues.posY, z: tweenValues.posZ});
+			camera.position.set(tweenValues.posX, tweenValues.posY, tweenValues.posZ);
 			
 			// Quaternion
 			THREE.Quaternion.slerp(originalQuaternion, destinationQuaternion, slerpedQuaternion, tweenValues.slerpT);
-			slerpedQuaternion.normalize();
-			Object.assign(camera.quaternion, slerpedQuaternion);
+			camera.setRotationFromQuaternion(slerpedQuaternion.normalize());
 		});
 	ongoingFocusTween && ongoingFocusTween.stop();
 	ongoingFocusTween = tween;
 	tween.start();
-}
+};
+
+const computeMarginToSprite = (spriteHeight, ratio) => {
+	console.log('spriteHeight', spriteHeight);
+	const visibleHeight = spriteHeight / ratio;
+	return visibleHeight / 2 * Math.tan((camera.fov * Math.PI / 180) / 2);
+};
 
 let wheelTimer;
 
@@ -217,9 +235,9 @@ renderer.domElement.addEventListener('wheel', event => {
 	
 	clearTimeout(wheelTimer);
 	wheelTimer = setTimeout(() => {
-			flyControls.moveState.forward = 0;
-			flyControls.moveState.back = 0;
-			flyControls.updateMovementVector();
+		flyControls.moveState.forward = 0;
+		flyControls.moveState.back = 0;
+		flyControls.updateMovementVector();
 	}, 200);
 });
 
