3e1c6a93c3256e1f16a6e077753bd0fdab7abe51
Shiori <shio.yama520@gmail.com>
2018-03-31 05:57:25 +0000

    combined zoom in and zoom out

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 - Make click easier
 
-# Bugs
+# Problems
 
 - Sprites can overlap
 
-- Strange camera rotation
 - Should the camera rotation restored when leaving the focus mode?
+
+# Camera Rotation
+
+When adjusting ```camera.up```, modifying camera's rotation or its quaternion doesn't matter.
+
+Slerp method have the same rotation problem as when I didn't use slerp. Ask Roger to test with cones.
--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ const HOVERED_SPRITE_COLOR = 0xe57373;
 // Size
 const SPRITE_MAX_SIZE = 4;
 // Fog density
-const DEFAULT_FOG_DENSITY = 0;//0.02;
-const FOG_DENSITY_IN_FOCUS_MODE = 0; //0.2;
+const DEFAULT_FOG_DENSITY = 0.02;
+const FOG_DENSITY_IN_FOCUS_MODE = 0.2;
 // Animation
 const FLY_CONTROLS_ROLL_SPEED = Math.PI / 5; // Math.PI / 6;
 const FOCUS_MODE_ANIMATION_DURATION = 800;
@@ -48,7 +48,8 @@ window.addEventListener('mousemove', event => {
 
 // Focus
 let focusedSprite;
-let previousCameraPosition;
+let previousCameraPosition = camera.position.clone();
+let previousCameraQuaternion = camera.quaternion.clone();
 let ongoingFocusTween;
 
 // Create and add Sprites
@@ -58,13 +59,13 @@ const subwayImgNames = ['american', 'banana_peppers', 'black_forest_ham', 'black
  'mustard', 'nine_grain_wheat', 'oil', 'oven_roasted_chicken', 'pickles', 'ranch', 'red_onions', 'spinach',
  'sweet_onion', 'sweet_onion_chicken_teriyaki', 'tomatoes', 'tuna', 'turkey_breast', 'vinaigrette', 'vinegar'];
 const pepperImgNames = ['square_pepper', 'horizontal_pepper', 'vertical_pepper'];
-//const spriteMaps = pepperImgNames.map(name => new THREE.TextureLoader().load(`img/${name}.png`));
+const spriteMaps = subwayImgNames.map(name => new THREE.TextureLoader().load(`img/${name}.png`));
 
 for (let i = 0; i < 500; i++) {
 	const sprite = new THREE.Sprite(new THREE.SpriteMaterial({
-		//map: spriteMaps[i % spriteMaps.length],
+		map: spriteMaps[i % spriteMaps.length],
 		//map: spriteMaps[Math.floor(Math.random() * spriteMaps.length)],
-		map: new THREE.TextureLoader().load(
+		/*map: new THREE.TextureLoader().load(
 			`img/${pepperImgNames[i % pepperImgNames.length]}.png`,
 			texture => {
 				if (texture.image.width >= texture.image.height) {
@@ -73,7 +74,7 @@ for (let i = 0; i < 500; i++) {
 					sprite.scale.set(texture.image.width * (SPRITE_MAX_SIZE / texture.image.height), SPRITE_MAX_SIZE);
 				}
 			}
-		),
+		),*/
 		color: DEFAULT_SPRITE_COLOR,
 		fog: true
 	}));
@@ -83,7 +84,12 @@ for (let i = 0; i < 500; i++) {
 }
 
 /*
-const coneGeometry = new THREE.ConeGeometry(3, 5, 5);
+const sprite = new THREE.Sprite(new THREE.SpriteMaterial({color: 0xff0000}));
+sprite.position.set(-2, 5, -20);
+scene.add(sprite);*/
+
+/*
+const coneGeometry = new THREE.ConeGeometry(2, 5, 8);
 for (let i = 0; i < 100; i++) {
 	const material = new THREE.MeshBasicMaterial({color: parseInt(Math.floor(Math.random() * (16 ** 6)).toString(16), 16)});
 	const cone = new THREE.Mesh(coneGeometry, material);
@@ -127,97 +133,77 @@ const render = () => {
 animate();
 
 // Click event
-window.addEventListener('mousedown', event => {
-	if (focusedSprite) {
-		leaveFocusMode(focusedSprite);
-		focusedSprite = null;
-	} else if (intersected) {
-		focusedSprite = intersected.object;
-		enterFocusMode(focusedSprite);
-	}
+renderer.domElement.addEventListener('mousedown', event => {
+  if (focusedSprite){ // zoom out
+    focusedSprite = null;
+    zoom(false);
+    toggleFlyControlsRolling(true);
+  } else if (intersected){ // zoom in
+  	focusedSprite.material.color.set(DEFAULT_SPRITE_COLOR);
+    focusedSprite = intersected.object;
+    previousCameraPosition = camera.position.clone();
+    previousCameraQuaternion = camera.quaternion.clone();
+    zoom(true);
+    toggleFlyControlsRolling(false);
+  }
 });
 
-const enterFocusMode = focusedSprite => {
-	focusedSprite.material.color.set(DEFAULT_SPRITE_COLOR);
-	toggleFlyControlsRolling(false);
-	
+const zoom = zoomIn => {
 	const tweenValues = {};
 	const tweenTarget = {};
 	
 	// Make the fog denser; the fog should not affect the focused sprite
-	focusedSprite.material.fog = false;
+	if (zoomIn) { focusedSprite.material.fog = false; }
 	tweenValues.fogDensity = scene.fog.density;
-	tweenTarget.fogDensity = FOG_DENSITY_IN_FOCUS_MODE;
+	tweenTarget.fogDensity = zoomIn ? FOG_DENSITY_IN_FOCUS_MODE : DEFAULT_FOG_DENSITY;
 	
-	// Move the camera
-	previousCameraPosition = camera.position.clone();
-	const distance = focusedSprite.position.clone().sub(camera.position);
-	const margin = distance.clone().normalize().multiplyScalar(3);
-	const targetPos = new THREE.Vector3().addVectors(camera.position, new THREE.Vector3().subVectors(distance, margin));
+  // Move the camera
+	const distance = new THREE.Vector3().subVectors(
+		zoomIn ? focusedSprite.position : previousCameraPosition,
+		camera.position
+	);
+	const margin = zoomIn ? distance.clone().normalize().multiplyScalar(3) : new THREE.Vector3();
+	const targetPos = new THREE.Vector3().addVectors(
+		camera.position,
+		new THREE.Vector3().subVectors(distance, margin)
+	);
 	Object.assign(tweenValues, {posX: camera.position.x, posY: camera.position.y, posZ: camera.position.z});
 	Object.assign(tweenTarget, {posX: targetPos.x, posY: targetPos.y, posZ: targetPos.z});
-	
-	// Rotate the camera
-	const slerpedQuaternion = new THREE.Quaternion();
-	const originalQuaternion = camera.quaternion.clone();
-  const tempCamera = camera.clone();
-  tempCamera.lookAt(focusedSprite.position);
   
-  const rotationTweenValue = {t: 0};
-  const rotationTween = new TWEEN.Tween(rotationTweenValue)
-  	.to({t: 1}, 1000)
-  	.easing(TWEEN.Easing.Quartic.Out);
-  rotationTween.onUpdate(() => {
-  	THREE.Quaternion.slerp(originalQuaternion, tempCamera.quaternion, slerpedQuaternion, rotationTweenValue.t);
-  	slerpedQuaternion.normalize();
-  	Object.assign(camera.quaternion, slerpedQuaternion);
-  });
-  rotationTween.start();
-	
-	// Tween everything
+  // Camera Rotation using Slerp
+  const originalQuaternion = camera.quaternion.clone();
+  let destinationQuaternion;
+  if (zoomIn) {
+	  const tempCamera = camera.clone();
+	  tempCamera.lookAt(focusedSprite.position);
+	  destinationQuaternion = tempCamera.quaternion.clone();
+  } else {
+  	destinationQuaternion = previousCameraQuaternion;
+  }
+  tweenValues.slerpT = 0;
+  tweenTarget.slerpT = 1;
+  let slerpedQuaternion = new THREE.Quaternion();
+  
+  // Tween everything
 	const tween = new TWEEN.Tween(tweenValues)
 		.to(tweenTarget, FOCUS_MODE_ANIMATION_DURATION)
-		.easing(TWEEN.Easing.Quartic.Out);
-	tween.onUpdate(() => {
-		scene.fog.density = tweenValues.fogDensity;
-		Object.assign(camera.position, {x: tweenValues.posX, y: tweenValues.posY, z: tweenValues.posZ});
-		//Object.assign(camera.rotation, {x: tweenValues.rotX, y: tweenValues.rotY, z: tweenValues.rotZ});
-		//Object.assign(camera.quaternion, {_x: tweenValues.qX, _y: tweenValues.qY, _z: tweenValues.qZ, _w: tweenValues.qW});
-	});
+		.easing(TWEEN.Easing.Quartic.Out)
+		.onUpdate(() => {
+			// Fog
+			scene.fog.density = tweenValues.fogDensity;
+			
+			// Position
+			Object.assign(camera.position, {x: tweenValues.posX, y: tweenValues.posY, z: tweenValues.posZ});
+			
+			// Quaternion
+			THREE.Quaternion.slerp(originalQuaternion, destinationQuaternion, slerpedQuaternion, tweenValues.slerpT);
+			slerpedQuaternion.normalize();
+			Object.assign(camera.quaternion, slerpedQuaternion);
+		});
 	ongoingFocusTween && ongoingFocusTween.stop();
 	ongoingFocusTween = tween;
 	tween.start();
-	
-	console.log(camera.quaternion);
-};
-
-const leaveFocusMode = focusedSprite => {
-	toggleFlyControlsRolling(true);
-	
-	const tweenValues = {};
-	const tweenTarget = {};
-	
-	// Reset the fog
-	focusedSprite.material.fog = true;
-	tweenValues.fogDensity = scene.fog.density;
-	tweenTarget.fogDensity = DEFAULT_FOG_DENSITY;
-	
-	// Move the camera back to the original position
-	Object.assign(tweenValues, {posX: camera.position.x, posY: camera.position.y, posZ: camera.position.z});
-	Object.assign(tweenTarget, {posX: previousCameraPosition.x, posY: previousCameraPosition.y, posZ: previousCameraPosition.z});
-	
-	// Tween everything
-	const tween = new TWEEN.Tween(tweenValues)
-		.to(tweenTarget, FOCUS_MODE_ANIMATION_DURATION)
-		.easing(TWEEN.Easing.Quartic.Out);
-	tween.onUpdate(() => {
-		scene.fog.density = tweenValues.fogDensity;
-		Object.assign(camera.position, {x: tweenValues.posX, y: tweenValues.posY, z: tweenValues.posZ});
-	});
-	ongoingFocusTween && ongoingFocusTween.stop();
-	ongoingFocusTween = tween;
-	tween.start();
-};
+}
 
 let wheelTimer;
 
